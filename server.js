03c924f46b9d18da16cdc5e370f69467e74326fa
coder-dom <giangtruong100702@gmail.com>
2024-11-10 12:40:40 +0700

    khởi tạo dự án expressjs

---
+++ server.js
@@ -0,0 +1,13 @@
+// khai báo port và khởi động server
+const app = require('./src/app');
+
+const PORT = 3055;
+
+const server = app.listen( PORT, () => {
+    console.log(`Web service eCommerce start with port ${PORT}`);
+});
+
+// process là quy trình trong nodejs, SIGINT nghĩa là khi ctrl + C
+process.on('SIGINT', () => {
+    server.close( () => console.log('Exit Server Express'));
+});
---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express();
+
+// init middlewares
+
+// init db
+
+// handling error
+
+
+module.exports = app;
