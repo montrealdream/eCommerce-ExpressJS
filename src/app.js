caed8e9810a4fa8c79037520da4a4e607cbd10ee
coder-dom <giangtruong100702@gmail.com>
2024-11-10 12:55:21 +0700

    hướng dẫn sử dụng morgan

--- src/app.js
+++ src/app.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // init middlewares
+// app.use(morgan("dev"));
+// app.use(morgan("combined"));
+// app.use(morgan("common"));
+// app.use(morgan("short"));
+app.use(morgan("tiny"));
 
 // init db
+
+// init routes
+app.get('/', (req, res, next) => {
+    return res.status(500).json({
+        message: 'Welcome Giang Truong!'
+    });
+});
 
 // handling error
 
