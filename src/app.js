4db4ab0b976017df27a38142f084392fe5f0132d
coder-dom <giangtruong100702@gmail.com>
2024-11-10 13:27:57 +0700

    cài đặt helmet và compression

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
+const {default: helmet} = require('helmet');
+const compression = require('compression');
 const app = express();
 
 // init middlewares
-// app.use(morgan("dev"));
-// app.use(morgan("combined"));
-// app.use(morgan("common"));
-// app.use(morgan("short"));
-app.use(morgan("tiny"));
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 
 // init db
 
